34fc401ce812edc53601f4f4e6f92ace0944971f
Chad Dollins <chad.dollins@gmail.com>
2014-05-08 23:59:41 -0500

    1. Removed model cacheing from index as it didn't honor fragment expiration when publisher or advertiser was modified. 2. Update logic in for fragment chache key to expire if pubishers or advertisers change

--- app/helpers/deals_helper.rb
+++ app/helpers/deals_helper.rb
@@ -1,7 +1,11 @@
 module DealsHelper
   def cache_key_for_deals
-    count          = Deal.count
-    max_updated_at = Deal.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    "products/all-#{count}-#{max_updated_at}"
+    d_count          = Deal.count
+    d_max_updated_at = Deal.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    a_count          = Advertiser.count
+    a_max_updated_at = Advertiser.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    p_count          = Publisher.count
+    p_max_updated_at = Publisher.maximum(:updated_at).try(:utc).try(:to_s, :number)    
+    "products/all-#{d_count}-#{d_max_updated_at}-#{a_count}-#{a_max_updated_at}-#{p_count}-#{p_max_updated_at}"
   end
 end
