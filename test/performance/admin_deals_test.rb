c563256b74366fea459c106537e9dd133c888b1e
Chad Dollins <chad.dollins@gmail.com>
2014-05-08 22:53:24 -0500

    Modifying performance test helper to use pre-populated database with 10K deal records to get better measurements in profiling with no record overhead.

--- test/performance/admin_deals_test.rb
+++ test/performance/admin_deals_test.rb
@@ -1,15 +1,9 @@
-require 'test_helper'
-require 'rails/performance_test_help'
+require 'performance_test_helper'
 
 class AdminDealsTest < ActionDispatch::PerformanceTest
   # Refer to the documentation for all available options
   # self.profile_options = { :runs => 5, :metrics => [:wall_time, :memory]
   #                          :output => 'tmp/performance', :formats => [:flat] }
-
-  def setup
-    @deals = FactoryGirl.create_list(:deal, 1000)
-  end
-  
 
   def test_admin_deals
     get '/deals'
