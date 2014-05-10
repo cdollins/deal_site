6c5737f5efe80228fc92dbb4be33900d210f4143
Chad Dollins <chad.dollins@gmail.com>
2014-05-10 18:26:33 -0500

    Setting up issue scenario 1. Created a validation for deal to not end before it starts 2. Added unit test for validation 3. Added unit test to import that has such issues

--- test/unit/deal_test.rb
+++ test/unit/deal_test.rb
@@ -4,6 +4,12 @@ class DealTest < ActiveSupport::TestCase
   test "factory should be sane" do
     assert FactoryGirl.build(:deal).valid?
   end
+  
+  test "offer can not expire before it begins" do
+    deal = FactoryGirl.build(:deal)
+    deal.end_at = deal.start_at - 1.week
+    assert !deal.valid?
+  end
 
   # I think this is a bad test and it fails sometimes
   test "over should honor current time" do
--- test/unit/import_test.rb
+++ test/unit/import_test.rb
@@ -20,6 +20,11 @@ McDonalds,10-5-2011,Not really food,22.00,44.98
 Arbys,10-8-2011,10-10-2011,More burgers,7.01,14.99
 END
 
+PUBLSIHER_NAME_3 = 'The Daily Beast'
+TESTFILE_3 = <<END
+Publisher,Start,finish,Name,cost,margin
+Burger King,10-4-2011,10-2-2011,Your way,25.91,50.92
+END
   def setup
     
   end
@@ -59,6 +64,12 @@ END
     assert_equal 2011, Deal.all.first.start_at.year
     assert_equal 1, Import.all.count
     assert_equal 1, ImportError.all.count
+  end
+  
+  test "publisher 3 has validation error name" do
+    Import.import(PUBLISHER_NAME_2, TESTFILE_2, "PmImportMap")
+    publisher = Publisher.first
+    assert_equal PUBLISHER_NAME_2, publisher.name
   end
   
 
